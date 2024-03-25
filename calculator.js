5c4f98148ee9be1ad2bfe874e749e3c62bac947a
bolids <35665172+bolids@users.noreply.github.com>
2024-03-25 12:49:25 +0100

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,33 @@
+class Calculator {
+    constructor() {
+    }
+
+    add(a, b) {
+        return a + b;
+
+    };
+
+    minus(a, b) {
+        return a - b;
+    };
+
+    divide(a, b) {
+        if (b == 0) {
+            throw new Error("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    /*divide(a, b) {
+        if (b==0) {
+            throw new Error ("Cannot divide by zero");
+        }
+        return a/b+1; //shell lead to failed test with correct expected result - it's for failed test status checking
+    }*/
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+};
+export default Calculator;
