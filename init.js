2aeaf20d7baca83ce12c7f7aa6b312e5f614bac3
bolids <35665172+bolids@users.noreply.github.com>
2024-03-25 12:36:38 +0100

    Create init.js

---
+++ init.js
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>... Smth before the script ...</p>
+
+  <script>
+    alert( 'Hello, world!' );
+  </script>
+
+  <p>...Smth after the script ...</p>
+
+</body>
+
+</html>
